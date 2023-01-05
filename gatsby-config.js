89e7078fc94cb0bbf29a94e66b94e8b9af229a13
Valerii Shevchenko <46709216+dadaismee@users.noreply.github.com>
2023-01-05 23:20:26 +0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `react4des`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
